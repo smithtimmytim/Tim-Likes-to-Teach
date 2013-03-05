f6f3bd7fe0795a5cf4e8f961e561aac8e5bf0321
Tim Smith <tim@timothybsmith.com>
2013-03-05 12:38:36 -0600

    Adding Google Analytics code

--- themes/v1/footer.php
+++ themes/v1/footer.php
@@ -39,6 +39,21 @@
   <script src="<?php bloginfo('template_url'); ?>/js/jquery.fitvids.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/stuff-ck.js"></script>
 
+  <!-- Google Analytics Code -->
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-39030671-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
+
 </body>
 </html>
 
