b453804331d2366ed34e33cc39401fff30a1574e
Tim Smith <tim@timothybsmith.com>
2013-03-11 12:58:45 -0500

    Taking out meta description

--- themes/v1/header.php
+++ themes/v1/header.php
@@ -3,7 +3,6 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title><?php bloginfo('name'); ?> » <?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
-  <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/global.css"/>
